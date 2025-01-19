37b7bf254e76a412528a8c559b60fcf1bfe25cfa
El Mehdi ASSALI <assalielmehdi@gmail.com>
2025-01-19 01:01:16 +0100

    First commit

---
+++ src/test/java/ma/assalielmehdi/sdt/eitc/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.assalielmehdi.sdt.eitc.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
